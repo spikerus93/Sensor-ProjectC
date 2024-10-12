fea789ee6981360b7fa30b8d905b794e9e593a53
master <spikerus93@rambler.ru>
2024-10-12 20:50:43 +0300

    Функция для получения данных от пользователя

--- sensor.c
+++ sensor.c
@@ -26,3 +26,32 @@ void AddRecord(struct sensor *info, int index, int year, int month, int day, int
     info[index].minute = minute;
     info[index].temperature = temperature;
 }
+
+// Функция для получения данных от пользователя
+int AddInfo(struct sensor *info)
+{
+    int counter = 0;
+    printf("Введите данные в следующем формате: YEAR;MONTH;DAY;HOUR;MINUTE;TEMPERATURE\n");
+
+    while (1)
+    {
+        printf("Информация для сенсора №%d:\n", counter + 1);
+
+        int year;
+        int month;
+        int day;
+        int hour;
+        int minute;
+        double temperature;
+
+        if (scanf("%d;%d;%d;%d;%d;%lf\n", &year, &month, &day, &hour, &minute, &temperature) != 6 || counter == SIZE)
+        {
+            // Если сканер вернул ошибку или количество объема данных достингуто, тогда выходим из цикла
+            printf("Неверный ввод...\n");
+            break;
+        }
+        AddRecord(info, counter++, year, month, day, hour, minute, temperature);
+    }
+
+    return counter;
+}
