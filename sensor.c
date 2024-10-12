6c0206bc0038839818a25259355dd2d100a338ae
master <spikerus93@rambler.ru>
2024-10-12 20:49:50 +0300

    Функция для добавления новой записи

--- sensor.c
+++ sensor.c
@@ -15,3 +15,14 @@ struct sensor
     int minute;
     double temperature;
 };
+
+// Функция для добавления новой записи
+void AddRecord(struct sensor *info, int index, int year, int month, int day, int hour, int minute, double temperature)
+{
+    info[index].year = year;
+    info[index].month = month;
+    info[index].day = day;
+    info[index].hour = hour;
+    info[index].minute = minute;
+    info[index].temperature = temperature;
+}
